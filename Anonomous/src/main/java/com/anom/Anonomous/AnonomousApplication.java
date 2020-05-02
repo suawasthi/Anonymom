36d58d67ed13acfd2e0496da97d63363c4052307
DEEPAK PATIL <suawasthi@gmail.com>
2020-05-02 11:34:57 +0530

    First project setup

---
+++ Anonomous/src/main/java/com/anom/Anonomous/AnonomousApplication.java
@@ -0,0 +1,14 @@
+package com.anom.Anonomous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonomousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonomousApplication.class, args);
+		System.out.println("ramesh");
+	}
+
+}
---
+++ Anonomous/src/test/java/com/anom/Anonomous/AnonomousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anom.Anonomous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonomousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
